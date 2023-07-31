ee168661c7f7485bd167200107e92af6ccca5863
MarieJadeMarcil <marie-jade.marcil@polymtl.ca>
2023-07-31 14:23:54 -0400

    setting server routes for client

--- flask-server/server.py
+++ flask-server/server.py
@@ -16,7 +16,9 @@ import DB
 
 # 1. Execute the virtual environment : source .venv/bin/activate
 # 2. Install the requiered packages : pip install -r requirements.txt
-
+class PatientEncoder(JSONEncoder):
+        def default(self, o):
+            return o.__dict__
 
 app = Flask(__name__)
 
@@ -34,24 +36,68 @@ try:
         print(' successfully connected to the pgAdmin database ! ')
     else:
         print(' not connected with database :( ')
+    
+    @app.route("/beds/<noadmsip>")
+    def searchPatient(noadmsip):   
+        patient = update_patient(noadmsip)
+        PatientEncoder().encode(patient)
+        patientJSONData = json.dumps(patient, indent=4, cls=PatientEncoder)
+        return(patientJSONData)
+    
+    @app.route("/dashboard/<kpi>/<noadmsip>/<timeFrame>")
+    def searchVariable(kpi, noadmsip, timeFrame):  
+        target_datetime = datetime.datetime.fromtimestamp(int(timeFrame)/1000)
+        cursor.execute("SELECT * FROM " + kpi +" WHERE noadmsip="+ noadmsip +" AND horodate > '" + str(target_datetime)[:19] +"' ORDER BY horodate DESC")     
+        allValues = cursor.fetchall()
+        PatientEncoder().encode(allValues)
+        allValuesJSONData = json.dumps(allValues, indent=4, cls=PatientEncoder)
+        return(allValuesJSONData)
 
+    @app.route("/cache/size")
+    def getCacheSize():  
+        file_name = "./Patients.db"
+        file_stats = os.stat(file_name)
+        size = file_stats.st_size / (1024 * 1024)
+        PatientEncoder().encode(size)
+        sizeJSONData = json.dumps(size, indent=4, cls=PatientEncoder)
+        return(sizeJSONData)
+    
+    @app.route("/db/<name>", methods=["DELETE"])
+    def db_delete():
+        cursor.execute("BEGIN;")
+        cursor.execute("DROP TABLE IF EXISTS PPC;")
+        cursor.execute("DROP TABLE IF EXISTS PICm;")
+        cursor.execute("DROP TABLE IF EXISTS LICOX;")
+        cursor.execute("DROP TABLE IF EXISTS Pupilles;")
+        cursor.execute("DROP TABLE IF EXISTS PVCm;")
+        cursor.execute("DROP TABLE IF EXISTS PAm;")
+        cursor.execute("DROP TABLE IF EXISTS ETCO2;")
+        cursor.execute("DROP TABLE IF EXISTS PaCO2;")
+        cursor.execute("DROP TABLE IF EXISTS Glycemie;")
+        cursor.execute("DROP TABLE IF EXISTS INR;")
+        cursor.execute("DROP TABLE IF EXISTS Plaquettes;")
+        cursor.execute("DROP TABLE IF EXISTS Temperature;")
+        cursor.execute("DROP TABLE IF EXISTS TeteLit;")
+        cursor.execute("DROP TABLE IF EXISTS Patient;")
+        cursor.execute("COMMIT;")
+        cursor.execute("COMMIT;")
+        return(' empty')
 
-    cursor.execute("BEGIN;")
-    cursor.execute("DROP TABLE IF EXISTS PPC;")
-    cursor.execute("DROP TABLE IF EXISTS PICm;")
-    cursor.execute("DROP TABLE IF EXISTS LICOX;")
-    cursor.execute("DROP TABLE IF EXISTS Pupilles;")
-    cursor.execute("DROP TABLE IF EXISTS PVCm;")
-    cursor.execute("DROP TABLE IF EXISTS PAm;")
-    cursor.execute("DROP TABLE IF EXISTS ETCO2;")
-    cursor.execute("DROP TABLE IF EXISTS PaCO2;")
-    cursor.execute("DROP TABLE IF EXISTS Glycemie;")
-    cursor.execute("DROP TABLE IF EXISTS INR;")
-    cursor.execute("DROP TABLE IF EXISTS Plaquettes;")
-    cursor.execute("DROP TABLE IF EXISTS Temperature;")
-    cursor.execute("DROP TABLE IF EXISTS TeteLit;")
-    cursor.execute("DROP TABLE IF EXISTS Patient;")
-    cursor.execute("COMMIT;")
+    def update_patient(noadmsip):
+        print(str(noadmsip))
+        cursor.execute('SELECT noadmsip FROM Patient;')  
+        allPatients = cursor.fetchall()
+        print(allPatients)
+        #Check if the patient already exists in the db
+        cursor.execute('SELECT lastLoadingTime FROM Patient WHERE noadmsip ='+ noadmsip +';')  
+        lastLoadingTime = cursor.fetchone()
+        if lastLoadingTime is not None:
+            print('not none')
+            limitDate = datetime.datetime.strptime(lastLoadingTime[0], '%Y-%m-%d %H:%M:%S')
+            print(limitDate)
+            cursor.execute("BEGIN;")
+            cursor.execute("UPDATE Patient SET lastLoadingTime = datetime('"+ str(dateNow) +"') WHERE noadmsip ="+ noadmsip +";")
+            cursor.execute("COMMIT;")
 
     # this is stored into my posgreSQL personal database created specificly for the developpement with a selected patient who was hospitalized in 2016
 
@@ -68,90 +114,90 @@ try:
     # 1. NEURO-MONITORING TARGETS
 
     cursor.execute("""CREATE TABLE IF NOT EXISTS
-    PPC(id INTEGER PRIMARY KEY, variable TEXT, noadmsip INTEGER, value INTEGER, datevariable TIMESTAMP, FOREIGN KEY(noadmsip) REFERENCES Patient(noadmsip))""")
+    PPC(id INTEGER PRIMARY KEY, kpi TEXT, noadmsip INTEGER, value INTEGER, horodate TIMESTAMP, FOREIGN KEY(noadmsip) REFERENCES Patient(noadmsip))""")
 
     cursor.execute("""CREATE INDEX IF NOT EXISTS idx_vent_noadmsip ON PPC (noadmsip)""")
-    cursor.execute("""CREATE INDEX IF NOT EXISTS idx_vent_datevariable ON PPC (datevariable)""")
+    cursor.execute("""CREATE INDEX IF NOT EXISTS idx_vent_horodate ON PPC (horodate)""")
 
     cursor.execute("""CREATE TABLE IF NOT EXISTS
-    PICm(id INTEGER PRIMARY KEY, variable TEXT, noadmsip INTEGER, value INTEGER, datevariable TIMESTAMP, FOREIGN KEY(noadmsip) REFERENCES Patient(noadmsip))""")
+    PICm(id INTEGER PRIMARY KEY, kpi TEXT, noadmsip INTEGER, value INTEGER, horodate TIMESTAMP, FOREIGN KEY(noadmsip) REFERENCES Patient(noadmsip))""")
 
     cursor.execute("""CREATE INDEX IF NOT EXISTS idx_picm_noadmsip ON PICm (noadmsip)""")
-    cursor.execute("""CREATE INDEX IF NOT EXISTS idx_picm_datevariable ON PICm (datevariable)""")
+    cursor.execute("""CREATE INDEX IF NOT EXISTS idx_picm_horodate ON PICm (horodate)""")
 
     cursor.execute("""CREATE TABLE IF NOT EXISTS
-    LICOX(id INTEGER PRIMARY KEY, variable TEXT, noadmsip INTEGER, value FLOAT, unitofmeasure TEXT, datevariable TIMESTAMP, FOREIGN KEY(noadmsip) REFERENCES Patient(noadmsip))""")
+    LICOX(id INTEGER PRIMARY KEY, kpi TEXT, noadmsip INTEGER, value FLOAT, unitofmeasure TEXT, horodate TIMESTAMP, FOREIGN KEY(noadmsip) REFERENCES Patient(noadmsip))""")
 
     cursor.execute("""CREATE INDEX IF NOT EXISTS idx_licox_noadmsip ON LICOX (noadmsip)""")
-    cursor.execute("""CREATE INDEX IF NOT EXISTS idx_licox_datevariable ON LICOX (datevariable)""")
+    cursor.execute("""CREATE INDEX IF NOT EXISTS idx_licox_horodate ON LICOX (horodate)""")
 
     cursor.execute("""CREATE TABLE IF NOT EXISTS
-    Pupilles(id INTEGER PRIMARY KEY, variable TEXT, noadmsip INTEGER, value TEXT, state TEXT, datevariable TIMESTAMP, FOREIGN KEY(noadmsip) REFERENCES Patient(noadmsip))""")
+    Pupilles(id INTEGER PRIMARY KEY, kpi TEXT, noadmsip INTEGER, value TEXT, state TEXT, horodate TIMESTAMP, FOREIGN KEY(noadmsip) REFERENCES Patient(noadmsip))""")
 
     cursor.execute("""CREATE INDEX IF NOT EXISTS idx_pupilles_noadmsip ON Pupilles (noadmsip)""")
-    cursor.execute("""CREATE INDEX IF NOT EXISTS idx_pupilles_datevariable ON Pupilles (datevariable)""")
+    cursor.execute("""CREATE INDEX IF NOT EXISTS idx_pupilles_horodate ON Pupilles (horodate)""")
 
 
     # 2. CARDIO-RESPIRATORY TARGETS
 
     cursor.execute("""CREATE TABLE IF NOT EXISTS
-    PVCm(id INTEGER PRIMARY KEY, variable TEXT, noadmsip INTEGER, value FLOAT, datevariable TIMESTAMP, FOREIGN KEY(noadmsip) REFERENCES Patient(noadmsip))""")
+    PVCm(id INTEGER PRIMARY KEY, kpi TEXT, noadmsip INTEGER, value FLOAT, horodate TIMESTAMP, FOREIGN KEY(noadmsip) REFERENCES Patient(noadmsip))""")
 
     cursor.execute("""CREATE INDEX IF NOT EXISTS idx_pvcm_noadmsip ON PVCm (noadmsip)""")
-    cursor.execute("""CREATE INDEX IF NOT EXISTS idx_pvcm_datevariable ON PVCm (datevariable)""")
+    cursor.execute("""CREATE INDEX IF NOT EXISTS idx_pvcm_horodate ON PVCm (horodate)""")
 
     cursor.execute("""CREATE TABLE IF NOT EXISTS
-    PAm(id INTEGER PRIMARY KEY, variable TEXT, noadmsip INTEGER, value FLOAT, datevariable TIMESTAMP, FOREIGN KEY(noadmsip) REFERENCES Patient(noadmsip))""")
+    PAm(id INTEGER PRIMARY KEY, kpi TEXT, noadmsip INTEGER, value FLOAT, horodate TIMESTAMP, FOREIGN KEY(noadmsip) REFERENCES Patient(noadmsip))""")
 
     cursor.execute("""CREATE INDEX IF NOT EXISTS idx_pam_noadmsip ON PAm (noadmsip)""")
-    cursor.execute("""CREATE INDEX IF NOT EXISTS idx_pam_datevariable ON PAm (datevariable)""")
+    cursor.execute("""CREATE INDEX IF NOT EXISTS idx_pam_horodate ON PAm (horodate)""")
 
     cursor.execute("""CREATE TABLE IF NOT EXISTS
-    ETCO2(id INTEGER PRIMARY KEY, variable TEXT, noadmsip INTEGER, value FLOAT, datevariable TIMESTAMP, FOREIGN KEY(noadmsip) REFERENCES Patient(noadmsip))""")
+    ETCO2(id INTEGER PRIMARY KEY, kpi TEXT, noadmsip INTEGER, value FLOAT, horodate TIMESTAMP, FOREIGN KEY(noadmsip) REFERENCES Patient(noadmsip))""")
 
     cursor.execute("""CREATE INDEX IF NOT EXISTS idx_etco2_noadmsip ON ETCO2 (noadmsip)""")
-    cursor.execute("""CREATE INDEX IF NOT EXISTS idx_etco2_datevariable ON ETCO2 (datevariable)""")
+    cursor.execute("""CREATE INDEX IF NOT EXISTS idx_etco2_horodate ON ETCO2 (horodate)""")
 
     cursor.execute("""CREATE TABLE IF NOT EXISTS
-    PaCO2(id INTEGER PRIMARY KEY, variable TEXT, noadmsip INTEGER, value FLOAT, datevariable TIMESTAMP, FOREIGN KEY(noadmsip) REFERENCES Patient(noadmsip))""")
+    PaCO2(id INTEGER PRIMARY KEY, kpi TEXT, noadmsip INTEGER, value FLOAT, horodate TIMESTAMP, FOREIGN KEY(noadmsip) REFERENCES Patient(noadmsip))""")
 
     cursor.execute("""CREATE INDEX IF NOT EXISTS idx_paco2_noadmsip ON PaCO2 (noadmsip)""")
-    cursor.execute("""CREATE INDEX IF NOT EXISTS idx_paco2_datevariable ON PaCO2 (datevariable)""")
+    cursor.execute("""CREATE INDEX IF NOT EXISTS idx_paco2_horodate ON PaCO2 (horodate)""")
 
 
     # 3. LABORATORY MONITORING TARGETS
 
     cursor.execute("""CREATE TABLE IF NOT EXISTS
-    Glycemie(id INTEGER PRIMARY KEY, variable TEXT, noadmsip INTEGER, value FLOAT, unitofmeasure TEXT, datevariable TIMESTAMP, FOREIGN KEY(noadmsip) REFERENCES Patient(noadmsip))""")
+    Glycemie(id INTEGER PRIMARY KEY, kpi TEXT, noadmsip INTEGER, value FLOAT, unitofmeasure TEXT, horodate TIMESTAMP, FOREIGN KEY(noadmsip) REFERENCES Patient(noadmsip))""")
 
     cursor.execute("""CREATE INDEX IF NOT EXISTS idx_glycemie_noadmsip ON Glycemie (noadmsip)""")
-    cursor.execute("""CREATE INDEX IF NOT EXISTS idx_glycemie_datevariable ON Glycemie (datevariable)""")
+    cursor.execute("""CREATE INDEX IF NOT EXISTS idx_glycemie_horodate ON Glycemie (horodate)""")
 
     cursor.execute("""CREATE TABLE IF NOT EXISTS
-    INR(id INTEGER PRIMARY KEY, variable TEXT, noadmsip INTEGER, value FLOAT, unitofmeasure TEXT, datevariable TIMESTAMP, FOREIGN KEY(noadmsip) REFERENCES Patient(noadmsip))""")
+    INR(id INTEGER PRIMARY KEY, kpi TEXT, noadmsip INTEGER, value FLOAT, unitofmeasure TEXT, horodate TIMESTAMP, FOREIGN KEY(noadmsip) REFERENCES Patient(noadmsip))""")
 
     cursor.execute("""CREATE INDEX IF NOT EXISTS idx_inr_noadmsip ON INR (noadmsip)""")
-    cursor.execute("""CREATE INDEX IF NOT EXISTS idx_inr_datevariable ON INR (datevariable)""")
+    cursor.execute("""CREATE INDEX IF NOT EXISTS idx_inr_horodate ON INR (horodate)""")
 
     cursor.execute("""CREATE TABLE IF NOT EXISTS
-    Plaquettes(id INTEGER PRIMARY KEY, variable TEXT, noadmsip INTEGER, value FLOAT, unitofmeasure TEXT, datevariable TIMESTAMP, FOREIGN KEY(noadmsip) REFERENCES Patient(noadmsip))""")
+    Plaquettes(id INTEGER PRIMARY KEY, kpi TEXT, noadmsip INTEGER, value FLOAT, unitofmeasure TEXT, horodate TIMESTAMP, FOREIGN KEY(noadmsip) REFERENCES Patient(noadmsip))""")
 
     cursor.execute("""CREATE INDEX IF NOT EXISTS idx_plaquettes_noadmsip ON Plaquettes (noadmsip)""")
-    cursor.execute("""CREATE INDEX IF NOT EXISTS idx_plaquettes_datevariable ON Plaquettes (datevariable)""")
+    cursor.execute("""CREATE INDEX IF NOT EXISTS idx_plaquettes_horodate ON Plaquettes (horodate)""")
 
     # 4. GENERAL SUPPORT MONITORING TARGETS
 
     cursor.execute("""CREATE TABLE IF NOT EXISTS
-    TeteLit(id INTEGER PRIMARY KEY, variable TEXT, noadmsip INTEGER, value TEXT, datevariable TIMESTAMP, FOREIGN KEY(noadmsip) REFERENCES Patient(noadmsip))""")
+    TeteLit(id INTEGER PRIMARY KEY, kpi TEXT, noadmsip INTEGER, value TEXT, horodate TIMESTAMP, FOREIGN KEY(noadmsip) REFERENCES Patient(noadmsip))""")
 
     cursor.execute("""CREATE INDEX IF NOT EXISTS idx_tetelit_noadmsip ON TeteLit (noadmsip)""")
-    cursor.execute("""CREATE INDEX IF NOT EXISTS idx_tetelit_datevariable ON TeteLit (datevariable)""")
+    cursor.execute("""CREATE INDEX IF NOT EXISTS idx_tetelit_horodate ON TeteLit (horodate)""")
 
     cursor.execute("""CREATE TABLE IF NOT EXISTS
-    Temperature(id INTEGER PRIMARY KEY, variable TEXT, noadmsip INTEGER, value TEXT, unitofmeasure TEXT, datevariable TIMESTAMP, FOREIGN KEY(noadmsip) REFERENCES Patient(noadmsip))""")
+    Temperature(id INTEGER PRIMARY KEY, kpi TEXT, noadmsip INTEGER, value TEXT, unitofmeasure TEXT, horodate TIMESTAMP, FOREIGN KEY(noadmsip) REFERENCES Patient(noadmsip))""")
 
     cursor.execute("""CREATE INDEX IF NOT EXISTS idx_temp_noadmsip ON Temperature (noadmsip)""")
-    cursor.execute("""CREATE INDEX IF NOT EXISTS idx_temp_datevariable ON Temperature (datevariable)""")
+    cursor.execute("""CREATE INDEX IF NOT EXISTS idx_temp_horodate ON Temperature (horodate)""")
 
     #Check if the patient already exists in the db
     cursor.execute('SELECT noadmsip FROM Patient WHERE noadmsip = 3563;')  
@@ -172,13 +218,13 @@ try:
             for index, row in df.iterrows():
                 values = ', '.join(f"'{value}'" for value in row)
                 if table == 'LICOX' or table == 'Glycemie' or table == 'Temperature' or table == 'INR' or table == 'Plaquettes':
-                    cursor.execute(f"INSERT INTO {table} (id, variable, noadmsip, value, unitofmeasure, datevariable) VALUES ({values});")
+                    cursor.execute(f"INSERT INTO {table} (id, kpi, noadmsip, value, unitofmeasure, horodate) VALUES ({values});")
                     cursor.execute("COMMIT;" )#end transaction
                 elif table == 'Pupilles':
-                    cursor.execute(f"INSERT INTO {table} (id, variable, noadmsip, value, state, datevariable) VALUES ({values});")
+                    cursor.execute(f"INSERT INTO {table} (id, kpi, noadmsip, value, state, horodate) VALUES ({values});")
                     cursor.execute("COMMIT;" )#end transaction
                 else:
-                    cursor.execute(f"INSERT INTO {table} (id, variable, noadmsip, value, datevariable) VALUES ({values});")
+                    cursor.execute(f"INSERT INTO {table} (id, kpi, noadmsip, value, horodate) VALUES ({values});")
                     cursor.execute("COMMIT;" )#end transaction
             
             print(' all data inserted in database :) ')
