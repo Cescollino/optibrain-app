93a7c278afa994c11261d72a7257207831a53d2e
Marie-Jade Marcil <marie-jade.marcil@polymtl.ca>
2023-08-05 10:43:30 -0400

    Fetching data cleaner with Axios

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,50 +5,50 @@ import { createTheme } from "@mui/material/styles";
 import { useEffect, useMemo, useState } from "react";
 import { themeSettings } from "@/theme";
 import Routes from "@/Routes"
-import { PATIENTS } from "@/api/kpiService";
 import { KpiData, IPatient, PatientRecordData } from "@/state/types";
 import { PatientStatus, StatusColor } from '@/types/patientState'
+import { getApi } from "./api/kpiService";
+import axios, { AxiosError } from "axios";
 
 function App() {
 
   /* Noadmsip : Numéro d'admission soins intensifs pédiatriques */
+  const [ isFeching, setIsFetching ] = useState(true);
   const [selectedPatient, setSelectedPatient] = useState<IPatient | null>(null);
+
   const [patientsData, setPatientsData] = useState<IPatient[]>([]);
-  const [kpiData, setKpiData] = useState<KpiData[]>([]);
+  // const [kpiData, setKpiData] = useState<KpiData[]>([]);
   const [patientRecords, setRecords] = useState<PatientRecordData[]>([]);
 
-
-  // Fetch all the patients record from cache once
-  useEffect(() => {
-    const fetchAllPatientRecords = async () => {
-      try {
-          const data = await PATIENTS.getAll();
-          if(data)
-            console.log('data 0: ', data);
-            setPatientsData([...patientsData, ...data]);
-      } catch (err) {
-        console.log('Error fetching patients records data:', err);
+  const getData = () => getApi().then(
+    (res) => {
+      if (res.status === 200 && res.data) {
+        setPatientsData(res.data)
+        console.log(patientsData) 
+      } else {
+        console.log(res)
       }
-    };
-
-    fetchAllPatientRecords();
-  }, [selectedPatient]);
-
+  }
+  )
 
   useEffect(() => {
-    const records: PatientRecordData[] = patientsData.map((patient) => ({
-      patient,
-      scans: 'CT scan',
-      stayDays: 7,
-      affectedSystems: ['Brain'],
-      status: {
-        state: PatientStatus.CRITICAL,
-        color: StatusColor.RED,
-      },
-    }) as PatientRecordData); 
-    setRecords([...patientRecords, ...records]);
-    console.log(patientRecords);
-  }, [patientsData]);
+    getData()
+  }, []);
+
+  // useEffect(() => {
+  //   const records: PatientRecordData[] = patientsData.map((patient) => ({
+  //     patient,
+  //     scans: 'CT scan',
+  //     stayDays: 7,
+  //     affectedSystems: ['Brain'],
+  //     status: {
+  //       state: PatientStatus.CRITICAL,
+  //       color: StatusColor.RED,
+  //     },
+  //   }) as PatientRecordData); 
+  //   setRecords([...patientRecords, ...records]);
+  //   console.log(patientRecords);
+  // }, [patientsData]);
 
   // // Fetch the key point indicators from the cache database
   // useEffect(() => {
--- client/src/api/kpiService.ts
+++ client/src/api/kpiService.ts
@@ -1,111 +1,74 @@
 import { IPatient, KpiData } from '@/state/types';
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosError, AxiosResponse } from 'axios';
 
 const API_URL = "http://localhost:5000";
 const PATIENTS_PATH = `${API_URL}/patients`
 
 interface IPatientApiResponse {
-  data: IPatient | IPatient[];
+  status: number;
+  data?: IPatient[];
+  error?: AxiosError;
+}
+
+
+interface Params {
+  baseUrl: string
+headers : any
+method: string
 }
 
 // Utility function for date to age conversion
 function dateOfBirthToAgeFormat(dateOfBirth: string | undefined ): string | undefined  {
-  if (!dateOfBirth) return undefined;
-  const today = new Date();
-  const birthDate = new Date(dateOfBirth);
+if (!dateOfBirth) return undefined;
+const today = new Date();
+const birthDate = new Date(dateOfBirth);
 
-  let ageYear = today.getFullYear() - birthDate.getFullYear();
-  let ageMonth = today.getMonth() - birthDate.getMonth();
-  let ageDay = today.getDate() - birthDate.getDate();
+let ageYear = today.getFullYear() - birthDate.getFullYear();
+let ageMonth = today.getMonth() - birthDate.getMonth();
+let ageDay = today.getDate() - birthDate.getDate();
 
-  if (ageDay < 0) {
-    ageMonth--;
-    const daysInLastMonth = new Date(
-      today.getFullYear(),
-      today.getMonth(),
-      0
-    ).getDate();
-    ageDay += daysInLastMonth;
-  }
-
-  if (ageMonth < 0) {
-    ageYear--;
-    ageMonth += 12;
-  }
-
-  return `Age: ${ageYear}a ${ageMonth}m ${ageDay}j`;
-};
-
-export class Patient {
-
-  patient: IPatient;
-
-  public constructor(patient: IPatient) { 
-    this.patient = patient
-  }
-
-  *[Symbol.iterator](): IterableIterator<Patient> {
-    yield this;
-  }
-
-  get age(): string | undefined {
-    return dateOfBirthToAgeFormat(this.patient.dateOfBirth);
-  }
+if (ageDay < 0) {
+  ageMonth--;
+  const daysInLastMonth = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    0
+  ).getDate();
+  ageDay += daysInLastMonth;
 }
 
-export default class Patients implements Iterable<IPatient> {
-  private patients: IPatient[] = [];
-
-  constructor(patients: IPatient[]) {
-    if(patients)
-      this.patients = patients.map(patient => patient);
-  }
-
-  *[Symbol.iterator](): IterableIterator<IPatient> {
-    if(this.patients.length > 0)
-      for (const patient of this.patients) {
-        yield patient;
-      }
-  }
-
-  get(): IPatient[] {
-    return this.patients;
-  }
-
-  getByNoadmsip(noadmsip: number): IPatient | undefined {
-    return this.patients.find((patient) => patient.noadmsip === noadmsip);
-  }
+if (ageMonth < 0) {
+  ageYear--;
+  ageMonth += 12;
 }
 
-const instance =  axios.create({
-    baseURL: "http://localhost:5000",
-    headers: {
-      "Content-type": "application/json"
-    }
-});
+return `Age: ${ageYear}a ${ageMonth}m ${ageDay}j`;
 
-const responseBody = (response: AxiosResponse<IPatientApiResponse>) => {
-  const data = response.data;
-  console.log('DATA', data);
-
-  if (Array.isArray(data)) {
-    return new Patients(data).get();
-  }
-  return [];
 };
 
-const requests = {
-	get: (url: string) => instance.get(url).then(responseBody),
-  getAll: (url: string) => instance.get(url).then(responseBody),
-	post: (url: string, body: {}) => instance.post(url, body).then(responseBody),
-	put: (url: string, body: {}) => instance.put(url, body).then(responseBody),
-	delete: (url: string) => instance.delete(url).then(responseBody),
+const getConfig : Params = {
+  baseUrl: API_URL,
+      headers: {
+          "Authorization": ""
+      },
+  method: 'get'
 };
 
-export const PATIENTS = {
-	// getOne: (noadmsip: number): Promise<Patient> => requests.get(`${PATIENTS_PATH}/${noadmsip}`),
-  getAll: (): Promise<IPatient[]> => requests.getAll(PATIENTS_PATH),
-  // getAllKpis: (noadmsip: number): Promise<KpiData[]> => requests.getAll(`${PATIENTS_PATH}/${noadmsip}/kpis`),
-  // getAllKpisByTimeFrame: (noadmsip: number, timeframe: number):  Promise<KpiData[]> => requests.getAll(`${PATIENTS_PATH}/${noadmsip}/kpis/${timeframe}`),
-	// delete: (noadmsip: number): Promise<void> => requests.delete(`${PATIENTS_PATH}/${noadmsip}`),
+export const getApi = async (): Promise<IPatientApiResponse> =>{
+  return await axios({
+      ...getConfig,
+      url: `${getConfig.baseUrl}/${PATIENTS_PATH}`,
+  }).then ( (response) => {
+      console.log('Fetching data response : ', response)
+      return {
+          status: response.status,
+          data: response.data
+      } as IPatientApiResponse
+  }).catch((error: AxiosError) =>{
+      console.log('Error when fetching patients data : ',error)
+      return {
+          status: error.status,
+          error: error.response
+      } as IPatientApiResponse
+  })
 };
