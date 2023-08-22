10b62cb0620a84be6e18560c8fe11c3c79959d9d
Marie-Jade Marcil <marie-jade.marcil@polymtl.ca>
2023-08-22 14:25:30 -0400

    moved postgreSQL other way to connect to db in separate folder

--- flask-server/config.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-# TODO: Create a separate file to avoid uploading on your repository
-import postgresqlconfig as cfg
-
-# Scheme: "postgres+psycopg2://<USERNAME>:<PASSWORD>@<IP_ADDRESS>:<PORT>/<DATABASE_NAME>"
-
-DATABASE_URI = f"postgresql+psycopg2://{cfg.mysql['username']}:{cfg.mysql['password']}@{cfg.mysql['host']}:5432/{cfg.mysql['db']}"
